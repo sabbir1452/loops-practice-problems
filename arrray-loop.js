2a51c9c90e65218b1624f4554a7fb6ffd63c2a51
sabbir1452 <sabbirrahman1452@gmail.com>
2024-01-04 11:06:16 +0600

    first

--- arrray-loop.js
+++ arrray-loop.js
@@ -6,7 +6,14 @@
 // target: display every elements of an array
 var numbers = [45, 87, 89, 56, 32, 51, 25, 98, 900, 987];
 
-for (var i = 0; i < numbers.length; i++) {
-  var number = numbers[i];
-  console.log(number);
+// for (var i = 0; i < numbers.length; i++) {
+//   var number = numbers[i];
+//   console.log(number);
+// }
+
+var items = ["bottle", "mouse", "sunglass", "pen", "notebook"];
+
+for (var i = 0; i < items.length; i++) {
+  var item = items[i];
+  console.log(item);
 }
