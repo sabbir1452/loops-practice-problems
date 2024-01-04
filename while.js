44f16e3267351bc576b000fbeacbe53d7fdb7cc9
sabbir1452 <sabbirrahman1452@gmail.com>
2024-01-04 10:52:36 +0600

    first commit

---
+++ while.js
@@ -0,0 +1,26 @@
+// if(condition){
+
+// }
+
+//   roastGiven = roastGiven + 1;
+//   roastGiven += 1;
+
+/*
+1. Loop variable
+2. condition inside while
+3. Loop body
+4. Change
+*/
+
+var roastGiven = 0;
+while (roastGiven < 7) {
+  console.log("Roast Den , Please !!");
+  console.log(roastGiven);
+  roastGiven++;
+}
+
+var num = 0;
+while (num < 120) {
+  console.log("I am Shabbir");
+  num = num + 2;
+}
