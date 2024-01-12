c4b666d7e554b4f3dfbc6e0fc8eadb5498263032
sabbir1452 <sabbirrahman1452@gmail.com>
2024-01-12 09:59:01 +0600

    lsf;lajdsl

---
+++ apple-orange.js
@@ -0,0 +1,8 @@
+var moneyGiven = 1000;
+var applePrice = 400;
+var orangePrice = 300;
+var totalPrice = applePrice + orangePrice;
+console.log("total Price:", totalPrice);
+
+var getBack = moneyGiven - totalPrice;
+console.log("get Back :", getBack);
