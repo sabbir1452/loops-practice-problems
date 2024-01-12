71da9a985ba88dcc4b5acaf6030d9ad71b631a83
sabbir1452 <sabbirrahman1452@gmail.com>
2024-01-12 11:21:07 +0600

    alkdf

---
+++ grading.js
@@ -0,0 +1,19 @@
+var myMarks = 80;
+var tomMarks = 66;
+var janeMarks = 95;
+var peterMarks = 56;
+var jonsMarks = 40;
+
+var mark = 39;
+
+if (mark >= 80) {
+  console.log("A grade");
+} else if (mark > 60) {
+  console.log("B grade");
+} else if (mark >= 50) {
+  console.log("C grade");
+} else if (mark >= 40) {
+  console.log("D grade");
+} else if (mark <= 39) {
+  console.log("F grade");
+}
